299b62339b60a2a27f5656b44019ca112eb6aab4
flokesong_drumer <87555423+FlokeZa89@users.noreply.github.com>
2022-12-10 18:44:36 +0700

    add feature

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 # Bot Module
 import interactions
+from interactions import Button , ButtonStyle, ActionRow
 import datetime
 import pytz
 from dotenv import load_dotenv
 import os
 import database_worker
+from discord.ext import tasks
+import asyncio
+# import profile_genarator
 
 # GET TOKEN
 load_dotenv()
@@ -18,6 +22,55 @@ def removemake(auid):
     rm = auid[auid.find('@')+1:auid.find('>')]
     buid = rm.lstrip("!")
     return buid
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(interactions.ClientPresence(status=interactions.StatusType.ONLINE,activities=[interactions.PresenceActivity(name="with Arai Kor Dai Server",details="with Arai Kor Dai Server",type=interactions.PresenceActivityType.GAME)]))
+    members = await interactions.get(bot, interactions.Channel, object_id=1051088869703024730)
+    guild = await interactions.get(bot,interactions.Guild,object_id=798448521287434280)
+    await members.modify(name="╰✦ MEMBERS : {}".format(guild.member_count))
+
+@tasks.loop(hours=3)
+async def updatedate():
+    datech = await interactions.get(bot, interactions.Channel, object_id=1051070646341345301)
+    datex = datetime.datetime.now(pytz.timezone('Asia/Bangkok'))
+    date = datex.strftime("%d %B %Y")
+    await asyncio.sleep(1)
+    await datech.modify(name="╭✦ {}".format(date))
+
+@tasks.loop(minutes=30)
+async def updatetime():
+    members = await interactions.get(bot, interactions.Channel, object_id=1051088869703024730)
+    guild = await interactions.get(bot,interactions.Guild,object_id=798448521287434280)
+    await members.modify(name="╰✦ MEMBERS : {}".format(guild.member_count))
+
+
+@bot.command(
+    name="hidepublicchat",
+    description="ตั้งค่าการมองเห็นแชทเซิฟเวอร์",
+    scope=guild_ids,
+)
+async def settingchat(ctx: interactions.CommandContext):
+    req_role = [1048099693252575242]
+    user = ctx.author
+    tguid = ctx.member.roles 
+    role_public = await interactions.get(bot, interactions.Role, object_id=1048099693252575242)
+    if any(req in tguid for req in req_role):
+        await ctx.send(f"ช่องสาธารณะไม่ถูกซ่อนอีกต่อไป",ephemeral = True)
+        await user.remove_role(role_public)
+    else:
+        await ctx.send(f"ช่องสาธารณะถูกซ่อนแล้ว",ephemeral = True)
+        await user.add_role(role_public)
+
+
+@bot.command(
+    name="invite",
+    description="รับลิงค์เชิญเข้าเซิฟเวอร์ Arai Kor Dai",
+    scope=guild_ids,
+)
+async def invite(ctx: interactions.CommandContext):
+    await ctx.send(f"ลิงค์เชิญเข้าสู่เซิฟ Arai Kor Dai : https://discord.gg/g5aKhQvgnM",ephemeral = True)
 
 @bot.command(
     name="register",
@@ -78,6 +131,7 @@ async def reverse_transaction(ctx, trans_id: int):
     ],
 )
 async def transaction(ctx, mention: str):
+    
     global from_id,to_id,fdata,tdata
     from_id = str(ctx.member.user.id)
     to_id = str(mention)
@@ -103,6 +157,7 @@ async def transaction(ctx, mention: str):
 
 @bot.modal("transaction")
 async def modal_response(ctx, response: str):
+    await ctx.send("ยืนยันจำนวนเงินสำเร็จ",ephemeral = True)
     error = database_worker.transaction(from_id,to_id,fdata,tdata,response)
     if (error == "true"):
         await ctx.send("การดำเนินการโอนเงิน : จำนวน {} Dollars Samlore / สถานะ - สำเร็จ".format(response))
@@ -160,6 +215,7 @@ async def transaction(ctx):
 
 @bot.modal("transaction")
 async def modal_response(ctx, response: str):
+    await ctx.send("ยืนยันจำนวนเงินสำเร็จ",ephemeral = True)
     error = database_worker.transaction(from_id,to_id,fdata,tdata,response)
     if (error == "true"):
         await ctx.send("การดำเนินการโอนเงิน : จำนวน {} Dollars Samlore / สถานะ - สำเร็จ".format(response))
@@ -199,14 +255,22 @@ async def embed(ctx: interactions.CommandContext):
     embed = interactions.Embed(timestamp=datetime.datetime.now(pytz.timezone('Asia/Bangkok')))
     data = database_worker.getdata(ctx.author.id)
     if (data != "false"):
-        cdata = data[0]
-        if cdata[4] == "a":
-            rank = "ผู้ดูแลระบบ"
-            point = "ไม่จำกัด"
-        if cdata[4] == "m":
-            rank = "สมาชิกทั่วไป"
-            point = cdata[3]
-    else:
+        try:
+            cdata = data[0]
+            if cdata != ():
+                if cdata[4] == "a":
+                    rank = "ผู้ดูแลระบบ"
+                    point = "ไม่จำกัด"
+                if cdata[4] == "m":
+                    rank = "สมาชิกทั่วไป"
+                    point = cdata[3]
+            else:
+                point = "Disabled"
+                rank = "Unknown"
+        except:
+            point = "Disabled"
+            rank = "Unknown"
+    if (data == "false"):
         point = "Disabled"
         rank = "Unknown"
     embed.set_author(name=str(ctx.member.user.username + "#" + ctx.member.user.discriminator), icon_url=ctx.author.get_avatar_url())
@@ -218,5 +282,7 @@ async def embed(ctx: interactions.CommandContext):
     embed.add_field(name="แรงค์", value=rank, inline=True)
     embed.set_footer(text="Araikordai Project | Google Link : In Development")
     await ctx.send(embeds=embed)
+
+updatedate.start()
 
 bot.start()
